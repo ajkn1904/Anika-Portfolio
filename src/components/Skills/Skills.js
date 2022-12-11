928bf4edf6b634c5d3a184076419a6454309ba97
ajkn1904 <ajknishat@gmail.com>
2022-12-12 00:01:21 +0600

    updated

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Flip from 'react-reveal/Flip';
+import Fade from 'react-reveal/Reveal';
 import icon1 from '../../icons/icon 1.png'
 import icon2 from '../../icons/icon 2.png'
 import icon3 from '../../icons/icon 3.png'
@@ -18,26 +20,58 @@ import icon15 from '../../icons/icon 15.png'
 const Skills = () => {
     return (
         <section id="skills" data-theme="cupcake" className='p-16 font-serif'>
+            <Fade bottom>
             <h1 className='text-center font-bold text-4xl my-10'>My Key SKILLS</h1>
             <div className=' w-[70%] mx-auto grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 gap-6 py-12'>
 
-            
+            <Flip top>
             <img src={icon1} className='h-20 w-16' alt="" />
+            </Flip>
+            <Flip top>
             <img src={icon2} className='h-20 w-16' alt="" />
+            </Flip>
+            <Flip top>
             <img src={icon3} className='h-20 w-16' alt="" />
+            </Flip>
+            <Flip top>
             <img src={icon4} className='h-20 w-16' alt="" />
+            </Flip>
+            <Flip top>
             <img src={icon5} className='h-20 w-16' alt="" />
+            </Flip>
+            <Flip top>
             <img src={icon6} className='h-20 w-16' alt="" />
+            </Flip>
+            <Flip top>
             <img src={icon7} className='h-20 w-16' alt="" />
+            </Flip>
+            <Flip top>
             <img src={icon8} className='h-20 w-16' alt="" />
+            </Flip>
+            <Flip top>
             <img src={icon9} className='h-20 w-16' alt="" />
+            </Flip>
+            <Flip top>
             <img src={icon10} className='h-20 w-16' alt="" />
+            </Flip>
+            <Flip top>
             <img src={icon11} className='h-20 w-16' alt="" />
+            </Flip>
+            <Flip top>
             <img src={icon12} className='h-20 w-16' alt="" />
+            </Flip>
+            <Flip top>
             <img src={icon13} className='h-20 w-16' alt="" />
+            </Flip>
+            <Flip top>
             <img src={icon14} className='h-20 w-16' alt="" />
+            </Flip>
+            <Flip top>
             <img src={icon15} className='h-20 w-16' alt="" />
+            </Flip>
+
             </div>
+            </Fade>
         </section>
     );
 };
